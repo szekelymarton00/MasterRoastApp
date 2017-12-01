1a2290834686debe8a944052dcacd109a4950c65
Marci <szekelymarton00@gmail.com>
2017-12-01 11:27:30 +0100

    soundplayer done

--- app/src/main/java/com/example/ezzo/masterroast/RoastActivity.java
+++ app/src/main/java/com/example/ezzo/masterroast/RoastActivity.java
@@ -1,7 +1,10 @@
 package com.example.ezzo.masterroast;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RoastActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class RoastActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_roast);
+
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.cataclysm);
+
+        Button play_button = (Button) this.findViewById(R.id.playcata);
+
+        play_button.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                mp.start();
+            }
+        });
     }
+
+    
 }
