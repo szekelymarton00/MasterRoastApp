dbe710c7f62f8b22135a878488dace5761c4062b
Marci <szekelymarton00@gmail.com>
2017-12-01 13:03:29 +0100

    randomquote added

--- app/src/main/java/com/example/ezzo/masterroast/RoastActivity.java
+++ app/src/main/java/com/example/ezzo/masterroast/RoastActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RoastActivity extends AppCompatActivity {
 
@@ -13,6 +14,7 @@ public class RoastActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_roast);
 
@@ -22,8 +24,9 @@ public class RoastActivity extends AppCompatActivity {
         final MediaPlayer masterFirst = MediaPlayer.create(this, R.raw.azefirst);
         final MediaPlayer masterSecond = MediaPlayer.create(this, R.raw.azesecond);
 
-        Button play_button_first = (Button) this.findViewById(R.id.playfirstaze);
-        Button play_button_second = (Button) this.findViewById(R.id.playsecondaze);
+        final Button play_button_first = (Button) this.findViewById(R.id.playfirstaze);
+        final Button play_button_second = (Button) this.findViewById(R.id.playsecondaze);
+        final Button getRandomQuote = (Button) this.findViewById(R.id.randomquote);
 
         play_button_first.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -34,6 +37,12 @@ public class RoastActivity extends AppCompatActivity {
         play_button_second.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 masterSecond.start();
+            }
+        });
+
+        play_button_second.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getRandomQuote,randomquote,Toast.LENGTH_LONG);
             }
         });
     }
